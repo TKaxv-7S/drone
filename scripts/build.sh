cc34c41d5241f5e11a3266da17c974e4dc5fa0bb
TKaxv_7S <954067342@qq.com>
2023-11-28 21:58:28 +0800

    修改 部署配置

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+go env -w GO111MODULE=on
+go env -w GOPROXY=https://goproxy.cn,https://mirrors.aliyun.com/goproxy/,direct
+
 echo "building docker images for ${GOOS}/${GOARCH} ..."
 
 REPO="github.com/drone/drone"
 
 # compile the server using the cgo
-go build -ldflags "-extldflags \"-static\"" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
+go build -ldflags "-extldflags \"-static\"" -tags="nolimit" -o release/linux/${GOARCH}/drone-server ${REPO}/cmd/drone-server
